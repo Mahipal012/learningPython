ec5bb4325aecdd08de5db19add1cec6ab94c9f42
Mahipal012 <165192782+Mahipal012@users.noreply.github.com>
2025-01-27 15:39:53 +0000

    27january

---
+++ string.py
@@ -0,0 +1,73 @@
+# name = 'mahi'
+# lName = "choudhary"
+# full_data = """Hello my name is mahi
+#                 i am from didwan"""
+
+# print(name)
+# print(lName)
+# print(full_data)
+
+# village = "Khariya"
+# print(village[0])
+# print(village[0:4])
+# print(village[2:])
+# print(village[:4])
+# print(village[::-1])
+# del village
+# print(village)
+
+# name = 'mahi'
+# lName = '     choudhary'
+# age = 23 
+# pin = 341551
+# print(name + lName)
+# print(name , lName)
+
+# fullName = name.replace("ma", "s")
+# print(fullName)
+
+# print(len(name))
+# print(lName.strip())
+# print(lName.upper())
+# print(lName.lower())
+
+# fstr = f"my name is {name} i am {age} year old my pin is {pin}"
+# print(fstr)
+
+
+# yes = "hello"
+# print("hel" in yes)
+# print("hel" not in yes)
+
+# namelen = "hello how are you"
+# print(len(namelen))
+
+# touper = 'uppercase'
+# print(touper.upper())
+
+# repword = "hello i love you"
+
+# print(repword.replace("love", "like"))
+
+# splitInWords = "hello"
+# print(splitInWords.split(','))
+
+# listOfWord = ['hello', 'how', 'are', 'you']
+# joinWords = ' '.join(listOfWord)
+# print(joinWords)
+
+# containWord = "hello is you"
+# print("is" in containWord)
+# print(containWord.index("you"))
+
+# wordCount = "hello hello who is this Hello"
+
+# print(wordCount.count("o"))
+# print(wordCount.count("hello"))
+
+# nameStrip = "   mahi   "
+# print(nameStrip.strip())
+
+# checkN = "hello"
+# print(checkN.startswith('h'))
+# print(checkN.endswith('lo'))
