753e56ed646ea8b93a933b18875d790e445ac6cf
Mahipal012 <165192782+Mahipal012@users.noreply.github.com>
2025-01-28 15:17:58 +0000

    28january2025

---
+++ dict.py
@@ -0,0 +1,15 @@
+my_dict = {"name":"mahi", "age":23, "cast":"jat"}
+# print(my_dict)
+# rr = my_dict.get('name')
+# print(rr)
+# xy = my_dict.pop('name')
+# xz = my_dict.popitem()
+
+my_dict['village'] = "Khariya"
+# my_dict['age'] = 24
+
+# for key, value in my_dict.items():
+    # print(key,"-",value)
+
+# if "age" in my_dict:
+#     print("hello", )
---
+++ list.py
@@ -0,0 +1,43 @@
+my_list = [1, 2, 3, 4]
+# my_list.append(5)
+# my_list.insert(4, 44)
+# my_list.extend([22,33,44])
+# print(my_list)
+
+mylisttw = [True, "mahi", 33, 44.4]
+# print(mylisttw)
+# print(mylisttw[0])
+# print(mylisttw[2:])
+# print(mylisttw[:4])
+# print(mylisttw[2:4])
+# print(mylisttw[:-1])
+# print(mylisttw[::-1]) #reverse list
+# new_mylisttw = mylisttw.pop()
+# mylisttw.remove(1)
+# del mylisttw
+# del mylisttw[0]
+# print(mylisttw)
+
+# three_list = [1, 2, 3, 4, 5, 55]
+
+# for x, y in three_list:
+#     print(x)
+
+# if 2 in three_list:
+#     print("yes ya hoo")
+
+four_list = [2]*4
+# print(four_list)
+
+five_list_squre = [x**2 for x in range(5)]
+# print(five_list_squre)    
+
+max_list = [22,34,32,44,11,45,20]
+# max_value = max(max_list)
+# print(max_value)
+
+swap_list = [1, 2, 3, 4, 5]
+
+# swap_list[0], swap_list[-1] = swap_list[-1], swap_list[0] 
+# swap_list[0], swap_list[4] = swap_list[4], swap_list[0] 
+print(swap_list)
