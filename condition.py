3c57a17ee23400d93f54f9226cd7f84227d013e6
Mahipal012 <165192782+Mahipal012@users.noreply.github.com>
2025-01-29 15:36:55 +0000

    29

---
+++ condition.py
@@ -0,0 +1,57 @@
+# age = 23
+
+# if age > 30:
+#     print("you are adult")
+# else:
+#     print("you are teenager")
+
+#even odd
+num = 32
+
+# if num % 2 == 0:
+#     print("num is even", num)
+# else:
+#     print("num is odd", num)
+
+# good better best
+
+score = 50
+
+# if score >= 80:
+#     print("best score", score)
+# elif score >= 60:
+#     print("score is better", score)
+# else:
+#     print("score is good", )
+
+number = 75
+
+# if number > 50 and number % 2 == 0:
+#     print("number is right enter")
+# elif number > 50 or number % 2 == 0:
+#     print("one condition true")
+# else:
+#     print("both condition rong")
+
+#nested if
+
+num = 50
+
+# if num > 40 :
+#     if num % 2 == 0:
+#         print("number is grater then 40 and even")
+# else:
+#     print("number is different")
+
+# fruits = ["apple", "banana", "cherry"]
+
+# if "apple" in fruits:
+#     print("i have apple")
+
+name = "mahi_choudhary"
+
+# if name.startswith("m"):
+#     print("is right name")
+
+# if name.__contains__("_"):
+#     print("yes")
