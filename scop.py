64f12098a92e95709e3bd1d789b39c99998f70be
Mahipal012 <165192782+Mahipal012@users.noreply.github.com>
2025-02-04 14:33:52 +0000

    ed

---
+++ scop.py
@@ -0,0 +1,48 @@
+# namef = "mahi"
+
+# def name():
+#     names = "shiv"
+#     print("your name", names)
+
+# name()
+
+# age = 23
+# def page(num):
+#     age = 32
+#     print("your age is ", num)
+
+# print(age)
+
+# def num(age):
+#     age = 23
+#     return age
+
+# page = num(34)
+# print(page)
+
+
+# def first():
+#     year = 2025
+#     def second():
+#         # year = 2026
+#         print(year)
+#     # return second
+#     second()
+
+# y=first()
+# print(y)
+
+# def yes():
+#     aa = 45
+#     def yo():
+#         print(aa)
+#     yo()
+
+# yes()
+
+# def yes():
+#     print(age)
+#     def yo():
+#         age = 24
+#     yo()
+# yes()
